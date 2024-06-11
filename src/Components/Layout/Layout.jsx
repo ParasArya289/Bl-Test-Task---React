4666a83434f564e1844a991e96525f2bb563e830
Paras Arya <parasarya289@gmail.com>
2024-06-12 03:22:05 +0530

    feat: layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -5,6 +5,8 @@ import Navbar from "../Navbar/Navbar";
 const Layout = ({ children }) => {
   return (
     <main className="layout">
+      {/* <div className="layout__nav">
+      </div> */}
       <Navbar />
       <div className="children">{children}</div>
     </main>
